87128a4d5b86e29dfe787cad446370e4790b6529
Yannick Lemarechal <yannick.lemarechal@univ-brest.fr>
2014-07-02 13:57:09 +0200

    add JKISS32

--- maths/prng.cu
+++ maths/prng.cu
@@ -18,6 +18,41 @@
 #ifndef PRNG_CU
 #define PRNG_CU
 
+// JKISS 32-bit (period ~2^121=2.6x10^36), passes all of the Dieharder tests and the BigCrunch tests in TestU01
+__device__ float JKISS32(ParticleStack &stack, unsigned int id) {
+    int t;
 
+    /*
+    y ^= (y<<5);
+    y ^= (y>>7);
+    y ^= (y<<22);
+    t = z+w+c;
+    z = w;
+    c = t < 0;
+    w = t & 2147483647;
+    x += 1411392427;
+    */
+
+    stack.prng_state_2[id] ^= (stack.prng_state_2[id] << 5);
+    stack.prng_state_2[id] ^= (stack.prng_state_2[id] >> 7);
+    stack.prng_state_2[id] ^= (stack.prng_state_2[id] << 22);
+    t = stack.prng_state_3[id] + stack.prng_state_4[id] + stack.prng_state_5[id];
+    stack.prng_state_3[id] = stack.prng_state_4[id];
+    stack.prng_state_5[id] = t < 0;
+    stack.prng_state_4[id] = t & 2147483647;
+    stack.prng_state_1[id] += 1411392427;
+
+    // Instead to return value between [0, 1] we return value between [0, 1[
+
+    // For the double version use that
+    // return (double)(x+y+w) / 4294967296.0;  // UINT_MAX+1
+
+    // For the float version is more tricky
+    float temp= ((float)(stack.prng_state_1[id]+stack.prng_state_2[id]+stack.prng_state_4[id]) 
+    //        UINT_MAX         1.0  - float32_precision
+            / 4294967295.0) * (1.0f - 1.0f/(1<<23));
+
+    return temp;
+}
 
 #endif
