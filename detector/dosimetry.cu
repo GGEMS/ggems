c317e86223c73aaa6a38f399d2306deb487af160
Yannick Lemarechal <yannick.lemarechal@univ-brest.fr>
2014-07-02 14:05:13 +0200

    add atomicAdd function with double precision

--- detector/dosimetry.cu
+++ detector/dosimetry.cu
@@ -15,26 +15,9 @@
 //
 // GGEMS Copyright (C) 2013-2014 Julien Bert
 
-#ifndef STRUCTURES_H
-#define STRUCTURES_H
+#ifndef DOSIMETRY_CU
+#define DOSIMETRY_CU
 
-/**
- * \author Y. Lemaréchal
- * \brief AtomicAdd function for double precision
- *        From Nvidia web site
- */
-__device__ double atomicAddDouble(double* address, double val)
-{
-    unsigned long long int* address_as_ull =
-                              (unsigned long long int*)address;
-    unsigned long long int old = *address_as_ull, assumed;
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                        __double_as_longlong(val +
-                               __longlong_as_double(assumed)));
-    } while (assumed != old);
-    return __longlong_as_double(old);
-}
+
 
 #endif
