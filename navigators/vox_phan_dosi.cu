0a3f9403c0a7645b407d65d79c8b5aa90fdaeee7
Yannick Lemarechal <yannick.lemarechal@univ-brest.fr>
2015-12-11 14:24:48 +0100

    add dosi phantom

---
+++ navigators/vox_phan_dosi.cu
@@ -0,0 +1,418 @@
+// GGEMS Copyright (C) 2015
+
+/*!
+ * \file vox_phan_img_nav.cu
+ * \brief
+ * \author J. Bert <bert.jul@gmail.com>
+ * \version 0.1
+ * \date 18 novembre 2015
+ *
+ *
+ *
+ */
+
+#ifndef VOX_PHAN_DOSI_CU
+#define VOX_PHAN_DOSI_CU
+
+#include "vox_phan_dosi.cuh"
+
+////:: GPU Codes
+
+// Move particles to the voxelized volume
+__host__ __device__ void vox_phan_track_to_in_dosi(ParticlesData &particles, f32 xmin, f32 xmax,
+                                              f32 ymin, f32 ymax, f32 zmin, f32 zmax,
+                                              ui32 id) {
+
+    // Read position
+    f64xyz pos;
+    pos.x = particles.px[id];
+    pos.y = particles.py[id];
+    pos.z = particles.pz[id];
+
+    // Read direction
+    f64xyz dir;
+    dir.x = particles.dx[id];
+    dir.y = particles.dy[id];
+    dir.z = particles.dz[id];
+
+    f32 dist = hit_ray_AABB(pos, dir, xmin, xmax, ymin, ymax, zmin, zmax);
+
+    // the particle not hitting the voxelized volume
+    if (dist == FLT_MAX) {                            // TODO: Don't know why F32_MAX doesn't work...
+        particles.endsimu[id] = PARTICLE_FREEZE;
+        return;
+    } else {
+        // Check if the path of the particle cross the volume sufficiently
+        f32 cross = dist_overlap_ray_AABB(pos, dir, xmin, xmax, ymin, ymax, zmin, zmax);
+        if (cross < EPSILON3) {
+            particles.endsimu[id] = PARTICLE_FREEZE;
+            return;
+        }
+        // move the particle slightly inside the volume
+        pos = fxyz_add(pos, fxyz_scale(pos, dist+EPSILON6));
+
+        // TODO update tof
+        // ...
+    }
+
+    // set photons
+    particles.px[id] = pos.x;
+    particles.py[id] = pos.y;
+    particles.pz[id] = pos.z;
+
+}
+
+
+__host__ __device__ void vox_phan_dosi_track_to_out(ParticlesData &particles,
+                                               VoxVolumeData vol,
+                                               MaterialsTable materials,
+                                               PhotonCrossSectionTable photon_CS_table,
+                                               GlobalSimulationParametersData parameters,
+                                               DosimetryTable dosi,
+                                               ui32 part_id) {
+
+
+    // Read position
+    f32xyz pos;
+    pos.x = particles.px[part_id];
+    pos.y = particles.py[part_id];
+    pos.z = particles.pz[part_id];
+
+    // Read direction
+    f32xyz dir;
+    dir.x = particles.dx[part_id];
+    dir.y = particles.dy[part_id];
+    dir.z = particles.dz[part_id];
+
+    // Defined index phantom
+    f32xyz ivoxsize;
+    ivoxsize.x = 1.0 / vol.spacing_x;
+    ivoxsize.y = 1.0 / vol.spacing_y;
+    ivoxsize.z = 1.0 / vol.spacing_z;
+    ui16xyzw index_phantom;
+    index_phantom.x = ui16( (pos.x+vol.org_x) * ivoxsize.x );
+    index_phantom.y = ui16( (pos.y+vol.org_y) * ivoxsize.y );
+    index_phantom.z = ui16( (pos.z+vol.org_z) * ivoxsize.z );
+    index_phantom.w = index_phantom.z*vol.nb_vox_x*vol.nb_vox_y
+                      + index_phantom.y*vol.nb_vox_x
+                      + index_phantom.x; // linear index
+
+    // Get the material that compose this volume
+    ui16 mat_id = vol.values[index_phantom.w];
+
+    //// Find next discrete interaction ///////////////////////////////////////
+
+    photon_get_next_interaction(particles, parameters, photon_CS_table, mat_id, part_id);
+
+    f32 next_interaction_distance = particles.next_interaction_distance[part_id];
+    ui8 next_discrete_process = particles.next_discrete_process[part_id];
+
+    //// Get the next distance boundary volume /////////////////////////////////
+
+    f32 vox_xmin = index_phantom.x*vol.spacing_x;
+    f32 vox_ymin = index_phantom.y*vol.spacing_y;
+    f32 vox_zmin = index_phantom.z*vol.spacing_z;
+    f32 vox_xmax = vox_xmin + vol.spacing_x;
+    f32 vox_ymax = vox_ymin + vol.spacing_y;
+    f32 vox_zmax = vox_zmin + vol.spacing_z;
+
+    f32 boundary_distance = hit_ray_AABB(pos, dir, vox_xmin, vox_xmax,
+                                         vox_ymin, vox_ymax, vox_zmin, vox_zmax);
+
+    if (boundary_distance <= next_interaction_distance) {
+        next_interaction_distance = boundary_distance + EPSILON3; // Overshoot
+        next_discrete_process = GEOMETRY_BOUNDARY;
+    }
+
+    //// Move particle //////////////////////////////////////////////////////
+
+    pos = fxyz_add(pos, fxyz_scale(dir, next_interaction_distance));
+
+    // Update TOF - TODO
+    //particles.tof[part_id] += c_light * next_interaction_distance;
+
+    particles.px[part_id] = pos.x;
+    particles.py[part_id] = pos.y;
+    particles.pz[part_id] = pos.z;
+
+    // Stop simulation if out of the phantom
+    if (!test_point_AABB(pos, vol.xmin, vol.xmax, vol.ymin, vol.ymax, vol.zmin, vol.zmax)) {
+        particles.endsimu[part_id] = PARTICLE_FREEZE;
+        return;
+    }
+
+    //// Apply discrete process //////////////////////////////////////////////////
+
+    if (next_discrete_process != GEOMETRY_BOUNDARY) {
+        // Resolve discrete process
+        SecParticle electron = photon_resolve_discrete_process(particles, parameters, photon_CS_table,
+                                                               materials, mat_id, part_id);
+
+        //// Here e- are not tracked, and lost energy not drop
+        
+        /// TODO ADD ELECTRON NAV
+        ggems_atomic_add(dosi.edep,index_phantom.w ,electron.E);
+        ggems_atomic_add(dosi.edep_squared,index_phantom.w ,electron.E*electron.E);
+        
+        
+    }
+
+    //// Energy cut
+    if (particles.E[part_id] <= materials.electron_energy_cut[mat_id]) {
+        particles.endsimu[part_id] = PARTICLE_DEAD;
+        return;
+    }
+
+}
+
+// Device Kernel that move particles to the voxelized volume boundary
+__global__ void kernel_device_track_to_in_dosi(ParticlesData particles, f32 xmin, f32 xmax,
+                                            f32 ymin, f32 ymax, f32 zmin, f32 zmax) {
+
+    const ui32 id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (id >= particles.size) return;
+
+    vox_phan_track_to_in_dosi(particles, xmin, xmax, ymin, ymax, zmin, zmax, id);
+
+}
+
+// Host Kernel that move particles to the voxelized volume boundary
+void kernel_host_track_to_in_dosi(ParticlesData particles, f32 xmin, f32 xmax,
+                             f32 ymin, f32 ymax, f32 zmin, f32 zmax, ui32 id) {
+
+    vox_phan_track_to_in_dosi(particles, xmin, xmax, ymin, ymax, zmin, zmax, id);
+
+}
+
+// Device kernel that track particles within the voxelized volume until boundary
+__global__ void kernel_device_track_to_out_dosi(ParticlesData particles,
+                                           VoxVolumeData vol,
+                                           MaterialsTable materials,
+                                           PhotonCrossSectionTable photon_CS_table,
+                                           GlobalSimulationParametersData parameters,
+                                           DosimetryTable dosi) {
+
+    const ui32 id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (id >= particles.size) return;
+
+    // Stepping loop
+    while (particles.endsimu[id] != PARTICLE_DEAD && particles.endsimu[id] != PARTICLE_FREEZE) {
+        vox_phan_dosi_track_to_out(particles, vol, materials, photon_CS_table, parameters, dosi, id);
+    }
+
+}
+
+// Host kernel that track particles within the voxelized volume until boundary
+void kernel_host_track_to_out_dosi(ParticlesData particles,
+                              VoxVolumeData vol,
+                              MaterialsTable materials,
+                              PhotonCrossSectionTable photon_CS_table,
+                              GlobalSimulationParametersData parameters,
+                              DosimetryTable dosi,
+                              ui32 id) {
+
+    // Stepping loop
+    while (particles.endsimu[id] != PARTICLE_DEAD && particles.endsimu[id] != PARTICLE_FREEZE) {
+        vox_phan_dosi_track_to_out(particles, vol, materials, photon_CS_table, parameters, dosi, id);
+    }
+}
+
+////:: Privates
+
+// Copy the phantom to the GPU
+void VoxPhanDosi::m_copy_phantom_cpu2gpu() {
+
+    // Mem allocation
+    HANDLE_ERROR( cudaMalloc((void**) &phantom.volume.data_d.values, phantom.volume.data_h.number_of_voxels*sizeof(ui16)) );
+    // Copy data
+    HANDLE_ERROR( cudaMemcpy(phantom.volume.data_d.values, phantom.volume.data_h.values,
+                  phantom.volume.data_h.number_of_voxels*sizeof(ui16), cudaMemcpyHostToDevice) );
+
+    phantom.volume.data_d.nb_vox_x = phantom.volume.data_h.nb_vox_x;
+    phantom.volume.data_d.nb_vox_y = phantom.volume.data_h.nb_vox_y;
+    phantom.volume.data_d.nb_vox_z = phantom.volume.data_h.nb_vox_z;
+
+    phantom.volume.data_d.spacing_x = phantom.volume.data_h.spacing_x;
+    phantom.volume.data_d.spacing_y = phantom.volume.data_h.spacing_y;
+    phantom.volume.data_d.spacing_z = phantom.volume.data_h.spacing_z;
+
+    phantom.volume.data_d.org_x = phantom.volume.data_h.org_x;
+    phantom.volume.data_d.org_y = phantom.volume.data_h.org_y;
+    phantom.volume.data_d.org_z = phantom.volume.data_h.org_z;
+
+    phantom.volume.data_d.number_of_voxels = phantom.volume.data_h.number_of_voxels;
+    
+//     m_dose_calculator.m_copy_dosi_cpu2gpu();
+}
+
+bool VoxPhanDosi::m_check_mandatory() {
+
+    if (phantom.volume.data_h.nb_vox_x == 0 || phantom.volume.data_h.nb_vox_y == 0 || phantom.volume.data_h.nb_vox_z == 0 ||
+        phantom.volume.data_h.spacing_x == 0 || phantom.volume.data_h.spacing_y == 0 || phantom.volume.data_h.spacing_z == 0 ||
+        phantom.list_of_materials.size() == 0) {
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+////:: Main functions
+
+void VoxPhanDosi::track_to_in(Particles particles) {
+
+    if (m_params.data_h.device_target == CPU_DEVICE) {
+        ui32 id=0; while (id<particles.size) {
+            kernel_host_track_to_in_dosi(particles.data_h, phantom.volume.data_h.xmin, phantom.volume.data_h.xmax,
+                                                   phantom.volume.data_h.ymin, phantom.volume.data_h.ymax,
+                                                   phantom.volume.data_h.zmin, phantom.volume.data_h.zmax,
+                                                   id);
+            ++id;
+        }
+    } else if (m_params.data_h.device_target == GPU_DEVICE) {
+
+        dim3 threads, grid;
+        threads.x = m_params.data_h.gpu_block_size;
+        grid.x = (particles.size + m_params.data_h.gpu_block_size - 1) / m_params.data_h.gpu_block_size;
+
+        kernel_device_track_to_in_dosi<<<grid, threads>>>(particles.data_d, phantom.volume.data_h.xmin, phantom.volume.data_h.xmax,
+                                                     phantom.volume.data_h.ymin, phantom.volume.data_h.ymax,
+                                                     phantom.volume.data_h.zmin, phantom.volume.data_h.zmax);
+        cuda_error_check("Error ", " Kernel_VoxPhanDosi (track to in)");
+
+    }
+
+
+}
+
+void VoxPhanDosi::track_to_out(Particles particles, Materials materials, PhotonCrossSection photon_CS) {
+
+    if (m_params.data_h.device_target == CPU_DEVICE) {
+
+        ui32 id=0; while (id<particles.size) {
+           
+            kernel_host_track_to_out_dosi(particles.data_h, phantom.volume.data_h,
+                                     materials.data_h, photon_CS.data_h, m_params.data_h, m_dose_calculator->dose_h, id);
+            ++id;
+        }
+    } else if (m_params.data_h.device_target == GPU_DEVICE) {
+
+        dim3 threads, grid;
+        threads.x = m_params.data_h.gpu_block_size;
+        grid.x = (particles.size + m_params.data_h.gpu_block_size - 1) / m_params.data_h.gpu_block_size;
+
+        kernel_device_track_to_out_dosi<<<grid, threads>>>(particles.data_d, phantom.volume.data_d, materials.data_d,
+                                                      photon_CS.data_d, m_params.data_d,m_dose_calculator->dose_d);
+        cuda_error_check("Error ", " Kernel_VoxPhanDosi (track to out)");
+
+    }
+
+}
+
+
+void VoxPhanDosi::load_phantom(std::string file, std::string matfile)
+{
+
+    if (ImageReader::get_format(file) == "mhd") 
+    {
+    
+        load_phantom_from_mhd(file, matfile);
+        
+    }
+    else
+    {
+        
+        print_error("Unknown phantom format ... \n");
+        exit_simulation();
+        
+    }
+
+}
+
+void VoxPhanDosi::load_phantom_from_mhd(std::string mhdfile, std::string matfile)
+{
+
+    phantom.load_from_mhd(mhdfile, matfile);
+
+}
+
+void VoxPhanDosi::initialize(GlobalSimulationParameters params) {
+    // Check params
+    if (!m_check_mandatory()) {
+        print_error("VoxPhanDosi: missing parameters.");
+        exit_simulation();
+    }
+
+    // Params
+    m_params = params;
+
+    // Phantom name
+    phantom.set_name("VoxPhanDosi");
+
+    // Copy data to GPU
+    if (m_params.data_h.device_target == GPU_DEVICE) {
+        m_copy_phantom_cpu2gpu();
+    }
+
+}
+
+// Get list of materials
+std::vector<std::string> VoxPhanDosi::get_materials_list() {
+    return phantom.list_of_materials;
+}
+
+// Get data that contains materials index
+ui16* VoxPhanDosi::get_data_materials_indices() {
+    return phantom.volume.data_h.values;
+}
+
+// Get the size of data
+ui32 VoxPhanDosi::get_data_size() {
+    return phantom.volume.data_h.number_of_voxels;
+}
+
+
+void VoxPhanDosi::add_dosimetry_map()
+{
+    add_dosimetry_map(
+    phantom.volume.data_h.nb_vox_x,
+    phantom.volume.data_h.nb_vox_y,
+    phantom.volume.data_h.nb_vox_z,
+    
+    phantom.volume.data_h.spacing_x,
+    phantom.volume.data_h.spacing_y,
+    phantom.volume.data_h.spacing_z,
+    
+    phantom.volume.data_h.org_x,
+    phantom.volume.data_h.org_y,
+    phantom.volume.data_h.org_z);
+}
+
+
+
+void VoxPhanDosi::add_dosimetry_map(ui32xyz nvox,f32xyz spacing,f32xyz offset)
+{
+
+//     add_dosimetry_map(nvox.x,nvox.y,nvox.z,spacing.x,spacing.y,spacing.z,offset.x,offset.y,offset.z);
+    m_dose_calculator->initialize(nvox,spacing,offset);
+    
+//     std::cout<<*m_dose_calculator<<std::endl;
+
+}
+
+void VoxPhanDosi::print_dosimetry()
+{
+    std::cout<<*m_dose_calculator<<std::endl;
+}
+
+
+void VoxPhanDosi::add_dosimetry_map(ui32 nvox_x,ui32 nvox_y ,ui32 nvox_z ,f32 spacing_x ,f32 spacing_y ,f32 spacing_z ,f32 offset_x,f32 offset_y ,f32 offset_z)
+{
+
+    add_dosimetry_map( make_ui32xyz(nvox_x,nvox_y,nvox_z) , make_f32xyz(spacing_x,spacing_y,spacing_z), make_f32xyz(offset_x,offset_y,offset_z) );
+
+}
+
+
+
+#endif
---
+++ navigators/vox_phan_dosi.cuh
@@ -0,0 +1,126 @@
+// GGEMS Copyright (C) 2015
+
+/*!
+ * \file vox_phan_dosi.cuh
+ * \brief
+ * \author Y. Lemaréchal
+ * \version 0.1
+ * \date 18 novembre 2015
+ *
+ *
+ *
+ */
+
+#ifndef VOX_PHAN_DOSI_CUH
+#define VOX_PHAN_DOSI_CUH
+
+#include "global.cuh"
+#include "voxelized.cuh"
+#include "raytracing.cuh"
+#include "vector.cuh"
+#include "materials.cuh"
+#include "photon.cuh"
+#include "photon_navigator.cuh"
+#include "image_reader.cuh"
+#include "dosimetry_actor.cuh"
+
+class VoxPhanDosi {
+    public:
+        VoxPhanDosi() {m_dose_calculator = new DoseCalculator;}
+        ~VoxPhanDosi() {}
+
+        // Tracking from outside to the phantom broder
+        void track_to_in(Particles particles);
+        // Tracking inside the phantom until the phantom border
+        void track_to_out(Particles particles, Materials materials, PhotonCrossSection photon_CS);
+        
+        // Check format phantom file
+        void load_phantom(std::string phantomfile, std::string materialfile);  
+        
+        // Init
+        void initialize(GlobalSimulationParameters params);
+
+        // Get list of materials
+        std::vector<std::string> get_materials_list();
+        // Get data that contains materials index
+        ui16* get_data_materials_indices();
+        // Get the size of data (nb of voxels)
+        ui32 get_data_size();
+
+        inline std::string get_name(){return "VoxPhanDosi";};
+        
+        // Dosimetry map functions
+        void add_dosimetry_map(); // Withou arg dosemap = phan size and position
+        void add_dosimetry_map(ui32xyz,f32xyz,f32xyz);
+        void add_dosimetry_map(ui32,ui32,ui32,f32,f32,f32,f32,f32,f32);
+        
+        void write(std::string filename = "dosimetry.mhd"){
+            m_dose_calculator->write_dosi(filename);
+        }
+        
+
+        void print_dosimetry();
+    private:
+        void load_phantom_from_mhd(std::string, std::string);  
+        
+        VoxelizedPhantom phantom;
+    
+        bool m_check_mandatory();       
+        void m_copy_phantom_cpu2gpu();
+
+        GlobalSimulationParameters m_params;
+
+        DoseCalculator *m_dose_calculator;
+        
+        
+        friend std::ostream& operator<<(std::ostream& os, VoxPhanDosi& v)
+        {
+            os  << std::fixed << std::setprecision(2);
+            os  << "VoxPhanDosi parameters  : " << std::endl;
+
+            os  << "\t"  <<  "+"  << std::setfill('-') << std::setw(30) << "+" << std::endl;
+            os  << std::setfill(' ');
+            
+            os  << "\t"   << "|" 
+                << std::left  << std::setw(9) << "" 
+                << std::right << std::setw(5) << "X"
+                << std::right << std::setw(7) << "Y" 
+                << std::right << std::setw(7) << "Z"
+                << std::setw(2)<< "|" << std::endl;
+            
+            os  << "\t"   << "|" 
+                << std::left  << std::setw(9) << "Offset" 
+                << std::right << std::setw(5) << v.phantom.volume.data_h.org_x 
+                << std::right << std::setw(7) << v.phantom.volume.data_h.org_y
+                << std::right << std::setw(7) << v.phantom.volume.data_h.org_z
+                << std::setw(2)<< "|" << std::endl;
+               
+            os  << "\t"   << "|" 
+                << std::left  << std::setw(9) << "Spacing" 
+                << std::right << std::setw(5) << v.phantom.volume.data_h.spacing_x 
+                << std::right << std::setw(7) << v.phantom.volume.data_h.spacing_y  
+                << std::right << std::setw(7) << v.phantom.volume.data_h.spacing_z 
+                << std::setw(2)<< "|" << std::endl;
+                
+            os << "\t"   << "|" 
+                << std::left  << std::setw(9) << "Size" 
+                << std::right << std::setw(5) << v.phantom.volume.data_h.nb_vox_x 
+                << std::right << std::setw(7) << v.phantom.volume.data_h.nb_vox_y 
+                << std::right << std::setw(7) << v.phantom.volume.data_h.nb_vox_z 
+                << std::setw(2)<< "|" << std::endl;
+                
+            os << "\t"   <<  "+"  << std::setfill('-') << std::setw(30) << "+" << std::endl;
+
+            std::cout<<"  Associated ";
+            v.print_dosimetry();
+            
+            return os;
+            
+            
+        }   
+        
+        
+//         
+};
+
+#endif
