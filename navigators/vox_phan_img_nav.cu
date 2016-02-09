ad1c527b2ba6151b3b1ba5065357470e06f37083
Julien Bert <julien.bert@univ-brest.fr>
2016-02-09 08:53:12 +0100

    Some cleaning

--- navigators/vox_phan_img_nav.cu
+++ navigators/vox_phan_img_nav.cu
@@ -303,20 +303,20 @@ void VoxPhanImgNav::initialize ( GlobalSimulationParameters params )
     // Materials table
     if( !m_elements_filename.empty() )
     {
-      m_materials.load_elements_database( m_elements_filename );
+        m_materials.load_elements_database( m_elements_filename );
     }
     else
     {
-      m_materials.load_elements_database();
+        m_materials.load_elements_database();
     }
 
     if( !m_materials_filename.empty() )
     {
-      m_materials.load_materials_database( m_materials_filename );
+        m_materials.load_materials_database( m_materials_filename );
     }
     else
     {
-      m_materials.load_materials_database();
+        m_materials.load_materials_database();
     }
 
     m_materials.initialize ( m_phantom.list_of_materials, params );
@@ -327,12 +327,12 @@ void VoxPhanImgNav::initialize ( GlobalSimulationParameters params )
 
 void VoxPhanImgNav::set_elements( std::string filename )
 {
-  m_elements_filename = filename;
+    m_elements_filename = filename;
 }
 
 void VoxPhanImgNav::set_materials( std::string filename )
 {
-  m_materials_filename = filename;
+    m_materials_filename = filename;
 }
 
 
