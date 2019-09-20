3eb0190a5d767730f9539019359844f6aa3b0494
Didier BENOIT <didier.benoit13@gmail.com>
2019-09-20 14:05:59 +0200

    remove all files to begin new project

--- ggems.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-export GGEMSHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
-export GGEMSLIB=$GGEMSHOME/lib 
-export GGEMSINC=$GGEMSHOME/include
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GGEMS/lib
