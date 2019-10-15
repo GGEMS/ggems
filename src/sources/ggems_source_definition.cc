c2f517049e93353a7f5ce04ea03ec8dd595e528d
Didier BENOIT <didier.benoit13@gmail.com>
2019-10-15 19:28:19 +0200

    New Source in GGEMS

---
+++ src/sources/ggems_source_definition.cc
@@ -0,0 +1,14 @@
+/*!
+  \file ggems_source_definition.cc
+
+  \brief GGEMS class managing the source in GGEMS, every new sources in GGEMS
+  inherit from this class
+
+  \author Julien BERT <julien.bert@univ-brest.fr>
+  \author Didier BENOIT <didier.benoit@inserm.fr>
+  \author LaTIM, INSERM - U1101, Brest, FRANCE
+  \version 1.0
+  \date Tuesday October 15, 2019
+*/
+
+#include "GGEMS/sources/ggems_source_definition.hh"
