45ed29c8f5c0f2330b527adbe459012d4f0658ed
Didier Benoit <didier.benoit13@gmail.com>
2020-02-20 14:44:16 +0100

    simplify writting and beginning of smart pointer

--- benchmarks/CT_imaging/ct.py
+++ benchmarks/CT_imaging/ct.py
@@ -3,7 +3,6 @@ from ggems import *
 # ------------------------------------------------------------------------------
 # STEP 1: Choosing an OpenCL context
 opencl_manager.set_context_index(2)
-
 opencl_manager.print_infos()
 
 # ------------------------------------------------------------------------------
@@ -12,28 +11,28 @@ opencl_manager.print_infos()
 
 # ------------------------------------------------------------------------------
 # STEP 3: Setting GGEMS materials
-material_manager.set_materials(b"data/materials.dat")
+#material_manager.set_materials(b"data/materials.dat")
 
-material_manager.print_available_chemical_elements()
-material_manager.print_available_materials()
+#material_manager.print_available_chemical_elements()
+#material_manager.print_available_materials()
 
 # ------------------------------------------------------------------------------
 # STEP 4: Phantom, Navigator and System
 # First phantom
-phantom_1 = GGEMSVoxelizedPhantomNavigatorImagery()
-phantom_1.set_phantom_name(b"phantom_1")
-phantom_1.set_phantom_image(b"data/phantom_1.mhd")
-phantom_1.set_range_to_material(b"data/range_phantom_1.dat")
-phantom_1.set_geometry_tolerance(1.0, b"mm")
+#phantom_1 = GGEMSVoxelizedPhantomNavigatorImagery()
+#phantom_1.set_phantom_name(b"phantom_1")
+#phantom_1.set_phantom_image(b"data/phantom_1.mhd")
+#phantom_1.set_range_to_material(b"data/range_phantom_1.dat")
+#phantom_1.set_geometry_tolerance(1.0, b"mm")
 
 # Second phantom
-phantom_2 = GGEMSVoxelizedPhantomNavigatorImagery()
-phantom_2.set_phantom_name(b"phantom_2")
-phantom_2.set_phantom_image(b"data/phantom_2.mhd")
-phantom_2.set_range_to_material(b"data/range_phantom_2.dat")
-phantom_2.set_geometry_tolerance(0.1, b"mm")
+#phantom_2 = GGEMSVoxelizedPhantomNavigatorImagery()
+#phantom_2.set_phantom_name(b"phantom_2")
+#phantom_2.set_phantom_image(b"data/phantom_2.mhd")
+#phantom_2.set_range_to_material(b"data/range_phantom_2.dat")
+#phantom_2.set_geometry_tolerance(0.1, b"mm")
 
-phantom_manager.print_infos()
+#phantom_manager.print_infos()
 
 # ------------------------------------------------------------------------------
 # STEP 5: Physics Declaration
@@ -56,28 +55,28 @@ phantom_manager.print_infos()
 # ------------------------------------------------------------------------------
 # STEP 6: Source Declaration AJOUTER LES UNITES
 # First source
-xray_source_1 = GGEMSXRaySource()
-xray_source_1.set_source_name(b"xray_source_1")
-xray_source_1.set_source_particle_type(b"photon")
-xray_source_1.set_number_of_particles(8616350000)
-xray_source_1.set_position(-1000.0, 0.0, 0.0, b"mm")
-xray_source_1.set_rotation(0.0, 0.0, 0.0, b"deg")
-xray_source_1.set_beam_aperture(5.0, b"deg")
-xray_source_1.set_focal_spot_size(0.6, 1.2, 0.0, b"mm")
-xray_source_1.set_polyenergy(b"data/spectrum_120kVp_2mmAl.dat")
+#xray_source_1 = GGEMSXRaySource()
+#xray_source_1.set_source_name(b"xray_source_1")
+#xray_source_1.set_source_particle_type(b"photon")
+#xray_source_1.set_number_of_particles(8616350000)
+#xray_source_1.set_position(-1000.0, 0.0, 0.0, b"mm")
+#xray_source_1.set_rotation(0.0, 0.0, 0.0, b"deg")
+#xray_source_1.set_beam_aperture(5.0, b"deg")
+#xray_source_1.set_focal_spot_size(0.6, 1.2, 0.0, b"mm")
+#xray_source_1.set_polyenergy(b"data/spectrum_120kVp_2mmAl.dat")
 
 # Second source
-xray_source_2 = GGEMSXRaySource()
-xray_source_2.set_source_name(b"xray_source_2")
-xray_source_2.set_source_particle_type(b"photon")
-xray_source_2.set_number_of_particles(861635)
-xray_source_2.set_position(0.0, -1000.0, 0.0, b"mm")
-xray_source_2.set_rotation(0.0, 0.0, 0.0, b"deg")
-xray_source_2.set_beam_aperture(7.0, b"deg")
-xray_source_2.set_focal_spot_size(0.3, 0.5, 0.0, b"mm")
-xray_source_2.set_monoenergy(60.2, b"keV")
+#xray_source_2 = GGEMSXRaySource()
+#xray_source_2.set_source_name(b"xray_source_2")
+#xray_source_2.set_source_particle_type(b"photon")
+#xray_source_2.set_number_of_particles(861635)
+#xray_source_2.set_position(0.0, -1000.0, 0.0, b"mm")
+#xray_source_2.set_rotation(0.0, 0.0, 0.0, b"deg")
+#xray_source_2.set_beam_aperture(7.0, b"deg")
+#xray_source_2.set_focal_spot_size(0.3, 0.5, 0.0, b"mm")
+#xray_source_2.set_monoenergy(60.2, b"keV")
 
-source_manager.print_infos()
+#source_manager.print_infos()
 
 # ------------------------------------------------------------------------------
 # STEP 7: Detector/Digitizer Declaration
@@ -85,7 +84,7 @@ source_manager.print_infos()
 
 # ------------------------------------------------------------------------------
 # STEP X: GGEMS simulation parameters
-ggems_manager.set_seed(777)
+#ggems_manager.set_seed(777)
 
 # ggems.opencl_verbose(true/false)
 # ggems.phantoms_verbose(true/false)
@@ -97,7 +96,7 @@ ggems_manager.set_seed(777)
 # ggems.tracking_verbose(true/false)
 
 # Initializing the GGEMS simulation
-ggems_manager.initialize()
+#ggems_manager.initialize()
 
 # Start GGEMS simulation
 # ggems.run()
--- include/GGEMS/global/GGEMSOpenCLManager.hh
+++ include/GGEMS/global/GGEMSOpenCLManager.hh
@@ -4,8 +4,7 @@
 /*!
   \file GGEMSOpenCLManager.hh
 
-  \brief Singleton class storing all informations about OpenCL and managing
-  GPU/CPU contexts and kernels for GGEMS
+  \brief Singleton class storing all informations about OpenCL and managing GPU/CPU contexts and kernels for GGEMS
   IMPORTANT: Only 1 context has to be activated.
 
   \author Julien BERT <julien.bert@univ-brest.fr>
@@ -58,7 +57,6 @@ class GGEMS_EXPORT GGEMSOpenCLManager
       return instance;
     }
 
-  public:
     /*!
       \fn GGEMSOpenCLManager(GGEMSOpenCLManager const& opencl_manager) = delete
       \param opencl_manager - reference on the singleton
@@ -71,8 +69,7 @@ class GGEMS_EXPORT GGEMSOpenCLManager
       \param opencl_manager - reference on the singleton
       \brief Avoid assignement of the singleton by reference
     */
-    GGEMSOpenCLManager& operator=(GGEMSOpenCLManager const& opencl_manager)
-      = delete;
+    GGEMSOpenCLManager& operator=(GGEMSOpenCLManager const& opencl_manager) = delete;
 
     /*!
       \fn GGEMSOpenCLManager(GGEMSOpenCLManager const&& opencl_manager) = delete
@@ -86,10 +83,8 @@ class GGEMS_EXPORT GGEMSOpenCLManager
       \param opencl_manager - rvalue reference on the singleton
       \brief Avoid copy of the singleton by rvalue reference
     */
-    GGEMSOpenCLManager& operator=(GGEMSOpenCLManager const&& opencl_manager)
-      = delete;
+    GGEMSOpenCLManager& operator=(GGEMSOpenCLManager const&& opencl_manager) = delete;
 
-  public: // Clean memory of correctly
     /*!
       \fn void Clean(void)
       \brief Clean correctly OpenCL platform, device, context, command queue, event and kernel
@@ -98,32 +93,12 @@ class GGEMS_EXPORT GGEMSOpenCLManager
 
     /*!
       \fn GGbool IsReady(void) const
-      \param Checking if the OpenCL manager is ready, it means if a context is set
+      \brief Checking if the OpenCL manager is ready, it means if a context is set
     */
     GGbool IsReady(void) const;
 
-  public: // Error stream management
     /*!
-      \fn void CheckOpenCLError(GGint const& error, std::string const& class_name, std::string const& method_name) const
-      \param error - error index
-      \param class_name - name of the class
-      \param method_name - name of the method
-      \brief check the OpenCL error
-    */
-    void CheckOpenCLError(GGint const& error, std::string const& class_name,
-      std::string const& method_name) const;
-
-  private:
-    /*!
-      \fn std::string ErrorType(GGint const& error) const
-      \param error - error index from OpenCL library
-      \brief get the error description
-    */
-    std::string ErrorType(GGint const& error) const;
-
-  public: // OpenCL infos
-    /*!
-      \fn void PrintPlatformInfos() const
+      \fn void PrintPlatformInfos(void) const
       \brief print all the informations about the platform
     */
     void PrintPlatformInfos(void) const;
@@ -132,30 +107,8 @@ class GGEMS_EXPORT GGEMSOpenCLManager
       \fn void PrintDeviceInfos(void) const
       \brief print all informations about devices
     */
-   void PrintDeviceInfos(void) const;
+    void PrintDeviceInfos(void) const;
 
-    /*!
-      \fn void PrintBuildOptions(void) const
-      \brief print global build options used during kernel compilation
-    */
-   void PrintBuildOptions(void) const;
-
-  private: // Context management
-    /*!
-      \fn void CreateContext(void)
-      \brief Create a context for GPU or CPU
-    */
-    void CreateContext(void);
-
-  public:
-    /*!
-      \fn void ContextToActivate(GGuint const& context_id)
-      \param context_id - context index
-      \brief set the index of the context to activate
-    */
-    void ContextToActivate(GGuint const& context_id);
-
-  public:
     /*!
       \fn void PrintContextInfos(void) const
       \brief print infos about each context
@@ -169,123 +122,26 @@ class GGEMS_EXPORT GGEMSOpenCLManager
     void PrintActivatedContextInfos(void) const;
 
     /*!
-      \fn std::size_t GetContext() const
-      \brief return the activated context
-    */
-    inline cl::Context* GetContext(void) const
-    {
-      return p_contexts_act_.front();
-    };
-
-  private: // OpenCL command queue
-    /*!
-      \fn void CreateCommandQueue(void)
-      \brief create a command queue for each context
-    */
-    void CreateCommandQueue(void);
-
-  public:
-    /*!
       \fn void PrintCommandQueueInfos(void) const
       \brief print the informations about the command queue
     */
     void PrintCommandQueueInfos(void) const;
 
     /*!
-      \fn cl::CommandQueue* GetCommandQueue(void) const
-      \brief Return the command queue to activated context
+      \fn void PrintBuildOptions(void) const
+      \brief print global build options used during kernel compilation
     */
-    inline cl::CommandQueue* GetCommandQueue(void) const
-    {
-      return p_queues_act_.front();
-    }
+    void PrintBuildOptions(void) const;
 
-  private: // OpenCL event
     /*!
-      \fn void CreateEvent()
-      \brief creating an event for each context
+      \fn void ContextToActivate(GGuint const& context_id)
+      \param context_id - context index
+      \brief set the index of the context to activate
     */
-    void CreateEvent();
+    void ContextToActivate(GGuint const& context_id);
 
-  public:
     /*!
-      \fn cl::Event* GetEvent(void) const
-      \brief return an event to activated context
-    */
-    inline cl::Event* GetEvent(void) const
-    {
-      return p_event_act_.front();
-    }
-
-  public:
-    /*!
-      \fn cl::Kernel* CompileKernel(std::string const& kernel_filename, std::string const& kernel_name, GGchar* const p_custom_options = nullptr, GGchar* const p_additional_options = nullptr)
-      \param kernel_filename - filename where is declared the kernel
-      \param kernel_name - name of the kernel
-      \param p_custom_options - new compilation option for the kernel
-      \param p_additionnal_options - additionnal compilation option
-      \brief Compile the OpenCL kernel on the activated context
-      \return the pointer on the OpenCL kernel
-    */
-    cl::Kernel* CompileKernel(std::string const& kernel_filename,
-      std::string const& kernel_name, char* const p_custom_options = nullptr,
-      char* const p_additional_options = nullptr);
-
-  public: // OpenCL buffer handling
-    /*!
-      \fn cl::Buffer* Allocate(void* p_host_ptr, std::size_t size, cl_mem_flags flags)
-      \param p_host_ptr - pointer to buffer in host memory
-      \param size - size of the buffer in bytes
-      \param flags - mode to open the buffer
-      \brief Allocation of OpenCL memory
-      \return a pointer to an OpenCL buffer
-    */
-    cl::Buffer* Allocate(void* p_host_ptr, std::size_t size,
-      cl_mem_flags flags);
-
-    /*
-      \fn void Deallocate(cl::Buffer* p_buffer, std::size_t size)
-      \param p_buffer - pointer to a buffer
-      \param size - size of the buffer in bytes
-      \brief Deallocation of OpenCL memory
-    */
-    void Deallocate(cl::Buffer* p_buffer, std::size_t size);
-
-    /*
-      \fn T* GetDeviceBuffer(cl::Buffer* p_device_ptr, std::size_t const size) const
-      \tparam T - type of the returned pointer on host memory
-      \param p_device_ptr - pointer on device memory
-      \param size - size of region to map
-      \brief Get the device pointer on host to write on it. ReleaseDeviceBuffer must be used after this method!!!
-      \return the pointer on host memory on write/read mode
-    */
-    template <typename T>
-    T* GetDeviceBuffer(cl::Buffer* const p_device_ptr,
-      std::size_t const size) const;
-
-    /*
-      \fn void ReleaseDeviceBuffer(cl::Buffer* const p_device_ptr, T* p_host_ptr) const
-      \tparam T - type of host memory pointer to release
-      \param p_device_ptr - pointer on device memory
-      \param p_host_ptr - pointer on host memory mapped on device memory
-      \brief Get the device pointer on host to write on it. Mandatory after a
-      GetDeviceBufferWrite ou GetDeviceBufferRead!!!
-      \return the pointer on host memory on write mode
-    */
-    template <typename T>
-    void ReleaseDeviceBuffer(cl::Buffer* const p_device_ptr,
-      T* p_host_ptr) const;
-
-  private: // RAM manager
-    /*!
-      \fn void InitializeRAMManager()
-      \brief initialization of the RAM manager by context
-    */
-    void InitializeRAMManager(void);
-
-  public:
-    /*!
-      \fn void PrintRAMStatus() const
+      \fn void PrintRAMStatus(void) const
       \brief print the RAM memory status for each context
     */
     void PrintRAMStatus(void) const;
@@ -313,14 +169,125 @@ class GGEMS_EXPORT GGEMSOpenCLManager
       return p_device_global_mem_size_[context_index_];
     }
 
-  public:
+    /*!
+      \fn std::size_t GetContext(void) const
+      \brief return the activated context
+    */
+    inline cl::Context* GetContext(void) const
+    {
+      return p_contexts_act_.front();
+    };
+
+    /*!
+      \fn cl::CommandQueue* GetCommandQueue(void) const
+      \brief Return the command queue to activated context
+    */
+    inline cl::CommandQueue* GetCommandQueue(void) const
+    {
+      return p_queues_act_.front();
+    }
+
+    /*!
+      \fn cl::Event* GetEvent(void) const
+      \brief return an event to activated context
+    */
+    inline cl::Event* GetEvent(void) const
+    {
+      return p_event_act_.front();
+    }
+
+    /*!
+      \fn cl::Kernel* CompileKernel(std::string const& kernel_filename, std::string const& kernel_name, char* const custom_options = nullptr, char* const additional_options = nullptr)
+      \param kernel_filename - filename where is declared the kernel
+      \param kernel_name - name of the kernel
+      \param custom_options - new compilation option for the kernel
+      \param additionnal_options - additionnal compilation option
+      \brief Compile the OpenCL kernel on the activated context
+      \return the pointer on the OpenCL kernel
+    */
+    cl::Kernel* CompileKernel(std::string const& kernel_filename, std::string const& kernel_name, char* const custom_options = nullptr, char* const additional_options = nullptr);
+
+    /*!
+      \fn cl::Buffer* Allocate(void* host_ptr, std::size_t size, cl_mem_flags flags)
+      \param host_ptr - pointer to buffer in host memory
+      \param size - size of the buffer in bytes
+      \param flags - mode to open the buffer
+      \brief Allocation of OpenCL memory
+      \return a pointer to an OpenCL buffer
+    */
+    cl::Buffer* Allocate(void* host_ptr, std::size_t size, cl_mem_flags flags);
+
+    /*
+      \fn void Deallocate(cl::Buffer* buffer, std::size_t size)
+      \param buffer - pointer to a buffer
+      \param size - size of the buffer in bytes
+      \brief Deallocation of OpenCL memory
+    */
+    void Deallocate(cl::Buffer* buffer, std::size_t size);
+
+    /*
+      \fn T* GetDeviceBuffer(cl::Buffer* device_ptr, std::size_t const size) const
+      \tparam T - type of the returned pointer on host memory
+      \param p_device_ptr - pointer on device memory
+      \param size - size of region to map
+      \brief Get the device pointer on host to write on it. ReleaseDeviceBuffer must be used after this method!!!
+      \return the pointer on host memory on write/read mode
+    */
+    template <typename T>
+    T* GetDeviceBuffer(cl::Buffer* const device_ptr, std::size_t const size) const;
+
+    /*
+      \fn void ReleaseDeviceBuffer(cl::Buffer* const p_device_ptr, T* p_host_ptr) const
+      \tparam T - type of host memory pointer to release
+      \param p_device_ptr - pointer on device memory
+      \param p_host_ptr - pointer on host memory mapped on device memory
+      \brief Get the device pointer on host to write on it. Mandatory after a
+      GetDeviceBufferWrite ou GetDeviceBufferRead!!!
+      \return the pointer on host memory on write mode
+    */
+    template <typename T>
+    void ReleaseDeviceBuffer(cl::Buffer* const device_ptr, T* host_ptr) const;
+
     /*!
       \fn void DisplayElapsedTimeInKernel(std::string const& kernel_name) const
       \brief Compute and display elapsed time in kernel for an activated context
     */
     void DisplayElapsedTimeInKernel(std::string const& kernel_name) const;
 
+    /*!
+      \fn void CheckOpenCLError(GGint const& error, std::string const& class_name, std::string const& method_name) const
+      \param error - error index
+      \param class_name - name of the class
+      \param method_name - name of the method
+      \brief check the OpenCL error
+    */
+    void CheckOpenCLError(GGint const& error, std::string const& class_name, std::string const& method_name) const;
+
   private:
+    /*!
+      \fn void CreateContext(void)
+      \brief Create a context for GPU or CPU
+    */
+    void CreateContext(void);
+
+    /*!
+      \fn void CreateCommandQueue(void)
+      \brief create a command queue for each context
+    */
+    void CreateCommandQueue(void);
+
+    /*!
+      \fn void CreateEvent()
+      \brief creating an event for each context
+    */
+    void CreateEvent();
+
+    /*!
+      \fn void InitializeRAMManager()
+      \brief initialization of the RAM manager by context
+    */
+    void InitializeRAMManager(void);
+
     /*!
       \fn void CheckRAMMemory(std::size_t const& size)
       \param size - size in bytes to allocate
@@ -328,11 +295,19 @@ class GGEMS_EXPORT GGEMSOpenCLManager
     */
     void CheckRAMMemory(std::size_t const& size);
 
-  private: // Platforms
-    std::vector<cl::Platform> p_platforms_; /*!< Vector of platforms */
-    std::string *p_platform_vendor_; /*!< Vendor of the platform */
+    /*!
+      \fn std::string ErrorType(GGint const& error) const
+      \param error - error index from OpenCL library
+      \brief get the error description
+    */
+    std::string ErrorType(GGint const& error) const;
 
-  private: // Devices
+  private:
+    // Platforms
+    std::vector<cl::Platform> platforms_; /*!< Vector of platforms */
+    std::vector<std::string> platform_vendor_; /*!< Vendor of the platform */
+
+    // Devices
     std::vector<cl::Device*> p_devices_; /*!< Vector of pointers of devices */
     cl_device_type *p_device_device_type_; /*!< Type of device */
     std::string *p_device_vendor_; /*!< Vendor of the device */
@@ -380,63 +355,60 @@ class GGEMS_EXPORT GGEMSOpenCLManager
     GGulong *p_used_ram_; /*!< Memory RAM used by context */
 };
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 template <typename T>
-T* GGEMSOpenCLManager::GetDeviceBuffer(cl::Buffer* const p_device_ptr,
-  std::size_t const size) const
+T* GGEMSOpenCLManager::GetDeviceBuffer(cl::Buffer* const device_ptr, std::size_t const size) const
 {
-  GGcout("GGEMSOpenCLManager", "GetDeviceBuffer", 3)
-    << "Getting mapped memory buffer on OpenCL device..." << GGendl;
+  GGcout("GGEMSOpenCLManager", "GetDeviceBuffer", 3) << "Getting mapped memory buffer on OpenCL device..." << GGendl;
 
   GGint err = 0;
-  T* ptr = static_cast<T*>(p_queues_act_.front()->enqueueMapBuffer(
-    *p_device_ptr, CL_TRUE, CL_MAP_WRITE | CL_MAP_READ, 0, size, nullptr,
-    nullptr, &err));
+  T* ptr = static_cast<T*>(p_queues_act_.front()->enqueueMapBuffer(*device_ptr, CL_TRUE, CL_MAP_WRITE | CL_MAP_READ, 0, size, nullptr, nullptr, &err));
   CheckOpenCLError(err, "GGEMSOpenCLManager", "GetDeviceBuffer");
   return ptr;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 template <typename T>
-void GGEMSOpenCLManager::ReleaseDeviceBuffer(cl::Buffer* const p_device_ptr,
-  T* p_host_ptr) const
+void GGEMSOpenCLManager::ReleaseDeviceBuffer(cl::Buffer* const device_ptr, T* host_ptr) const
 {
-  GGcout("GGEMSOpenCLManager", "ReleaseDeviceBuffer", 3)
-    << "Releasing mapped memory buffer on OpenCL device..." << GGendl;
+  GGcout("GGEMSOpenCLManager", "ReleaseDeviceBuffer", 3) << "Releasing mapped memory buffer on OpenCL device..." << GGendl;
 
   // Unmap the memory
-  CheckOpenCLError(p_queues_act_.front()->enqueueUnmapMemObject(*p_device_ptr,
-    p_host_ptr), "GGEMSOpenCLManager", "ReleaseDeviceBuffer");
+  CheckOpenCLError(p_queues_act_.front()->enqueueUnmapMemObject(*device_ptr, host_ptr), "GGEMSOpenCLManager", "ReleaseDeviceBuffer");
 }
 
 /*!
   \fn GGEMSOpenCLManager* get_instance_ggems_opencl_manager(void)
   \brief Get the GGEMSOpenCLManager pointer for python user.
 */
-extern "C" GGEMS_EXPORT GGEMSOpenCLManager*
-  get_instance_ggems_opencl_manager(void);
+extern "C" GGEMS_EXPORT GGEMSOpenCLManager* get_instance_ggems_opencl_manager(void);
 
 /*!
-  \fn void print_infos_opencl_manager(GGEMSOpenCLManager* p_opencl_manager)
-  \param p_opencl_manager - pointer on the singleton
+  \fn void print_infos_opencl_manager(GGEMSOpenCLManager* opencl_manager)
+  \param opencl_manager - pointer on the singleton
   \brief Print information about OpenCL
 */
-extern "C" GGEMS_EXPORT void print_infos_opencl_manager(
-  GGEMSOpenCLManager* p_opencl_manager);
+extern "C" GGEMS_EXPORT void print_infos_opencl_manager(GGEMSOpenCLManager* opencl_manager);
 
 /*!
-  \fn void print_RAM_ggems_opencl_manager(GGEMSOpenCLManager* p_opencl_manager)
-  \param p_opencl_manager - pointer on the singleton
+  \fn void print_RAM_ggems_opencl_manager(GGEMSOpenCLManager* opencl_manager)
+  \param opencl_manager - pointer on the singleton
   \brief Print information about RAM in OpenCL context
 */
-extern "C" GGEMS_EXPORT void print_RAM_ggems_opencl_manager(
-  GGEMSOpenCLManager* p_opencl_manager);
+extern "C" GGEMS_EXPORT void print_RAM_ggems_opencl_manager(GGEMSOpenCLManager* opencl_manager);
 
 /*!
-  \fn void set_context_index_ggems_opencl_manager(GGEMSOpenCLManager* p_opencl_manager, uint32_t const context_id)
-  \param p_opencl_manager - pointer on the singleton
+  \fn void set_context_index_ggems_opencl_manager(GGEMSOpenCLManager* opencl_manager, uint32_t const context_id)
+  \param opencl_manager - pointer on the singleton
   \param context_id - index of the context
   \brief Set the context index to activate
 */
-extern "C" GGEMS_EXPORT void set_context_index_ggems_opencl_manager(
-  GGEMSOpenCLManager* p_opencl_manager, GGuint const context_id);
+extern "C" GGEMS_EXPORT void set_context_index_ggems_opencl_manager(GGEMSOpenCLManager* opencl_manager, GGuint const context_id);
 
 #endif // GUARD_GGEMS_GLOBAL_GGEMSOPENCLMANAGER_HH
