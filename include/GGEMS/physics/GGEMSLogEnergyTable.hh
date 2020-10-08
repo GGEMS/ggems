60812d10bff42f363dd9db73a1d1f68c9836404f
Didier Benoit <didier.benoit13@gmail.com>
2020-10-08 15:41:18 +0200

    License GPLv3 in files

--- include/GGEMS/physics/GGEMSLogEnergyTable.hh
+++ include/GGEMS/physics/GGEMSLogEnergyTable.hh
@@ -1,6 +1,24 @@
 #ifndef GUARD_GGEMS_PHYSICS_GGEMSLOGENERGYTABLE_HH
 #define GUARD_GGEMS_PHYSICS_GGEMSLOGENERGYTABLE_HH
 
+// ************************************************************************
+// * This file is part of GGEMS.                                          *
+// *                                                                      *
+// * GGEMS is free software: you can redistribute it and/or modify        *
+// * it under the terms of the GNU General Public License as published by *
+// * the Free Software Foundation, either version 3 of the License, or    *
+// * (at your option) any later version.                                  *
+// *                                                                      *
+// * GGEMS is distributed in the hope that it will be useful,             *
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+// * GNU General Public License for more details.                         *
+// *                                                                      *
+// * You should have received a copy of the GNU General Public License    *
+// * along with GGEMS.  If not, see <https://www.gnu.org/licenses/>.      *
+// *                                                                      *
+// ************************************************************************
+
 /*!
   \file GGEMSLogEnergyTable.hh
 
