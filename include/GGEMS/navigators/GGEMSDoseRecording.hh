1323c41d487ec0daed8b02a88aabc9bb9fa83280
Didier Benoit <didier.benoit@inserm.fr>
2021-01-28 00:52:49 +0100

    dosimetry debugging

--- include/GGEMS/navigators/GGEMSDoseRecording.hh
+++ include/GGEMS/navigators/GGEMSDoseRecording.hh
@@ -70,6 +70,7 @@ inline void dose_photon_tracking(global GGEMSDoseParams* dose_params, global GGi
   // Get index in dose map
   GGint3 dosel_id = convert_int3((*position - dose_params->border_min_xyz_) * dose_params->inv_size_of_dosels_);
   GGint global_dosel_id = dosel_id.x + dosel_id.y * dose_params->number_of_dosels_.x + dosel_id.z * dose_params->number_of_dosels_.x * dose_params->number_of_dosels_.y;
+
   atomic_add(&photon_tracking[global_dosel_id], 1);
 }
 
