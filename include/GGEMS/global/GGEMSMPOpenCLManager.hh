708db8ecd83ec234c2536923fd60063587371fb3
Didier Benoit <didier.benoit@inserm.fr>
2021-03-23 14:57:19 +0100

    Beginning of Multi-platform execution for GGEMS

---
+++ include/GGEMS/global/GGEMSMPOpenCLManager.hh
@@ -0,0 +1,34 @@
+#ifndef GUARD_GGEMS_GLOBAL_GGEMSMPOPENCLMANAGER_HH
+#define GUARD_GGEMS_GLOBAL_GGEMSMPOPENCLMANAGER_HH
+
+// ************************************************************************
+// * This file is part of GGEMS.                                          *
+// *                                                                      *
+// * GGEMS is free software: you can redistribute it and/or modify        *
+// * it under the terms of the GNU General Public License as published by *
+// * the Free Software Foundation, either version 3 of the License, or    *
+// * (at your option) any later version.                                  *
+// *                                                                      *
+// * GGEMS is distributed in the hope that it will be useful,             *
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+// * GNU General Public License for more details.                         *
+// *                                                                      *
+// * You should have received a copy of the GNU General Public License    *
+// * along with GGEMS.  If not, see <https://www.gnu.org/licenses/>.      *
+// *                                                                      *
+// ************************************************************************
+
+/*!
+  \file GGEMSMPOpenCLManager.hh
+
+  \brief Singleton class storing all informations about OpenCL and managing GPU/CPU devices, contexts, kernels, command queues and events. In GGEMS the strategy is 1 context = 1 device.
+
+  \author Julien BERT <julien.bert@univ-brest.fr>
+  \author Didier BENOIT <didier.benoit@inserm.fr>
+  \author LaTIM, INSERM - U1101, Brest, FRANCE
+  \version 1.0
+  \date Tuesday March 23, 2021
+*/
+
+#endif // GUARD_GGEMS_GLOBAL_GGEMSMPOPENCLMANAGER_HH
